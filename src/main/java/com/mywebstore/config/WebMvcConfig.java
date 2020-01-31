0a4dded879e79437565588fc7bac98f6c6c164a1
ranjan <ranjan1423@gmail.com>
2020-01-31 13:52:12 +0530

    removed web.xml and context.xml added java based configuration

---
+++ src/main/java/com/mywebstore/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.mywebstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.mywebstore")
+public class WebMvcConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver irvr = new InternalResourceViewResolver();
+		irvr.setViewClass(JstlView.class);
+		irvr.setPrefix("/WEB-INF/views/jsp/");
+		irvr.setPrefix(".jsp");
+		return irvr;
+	}
+
+}
